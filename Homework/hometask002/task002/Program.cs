4217aa98fa379fbc5f2ae7e48e4b4bf7a5f312b8
AnnPCMaibenben <ann.erma4kova@gmail.com>
2022-11-23 22:54:35 +0300

    second2 task

---
+++ Homework/hometask002/task002/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int number = 32679;
+
+if (number >= 100)
+{
+    while (number >= 1000)
+    {
+        number = number / 10;
+    }
+    Console.WriteLine(number % 10);
+}
+else
+{
+    Console.WriteLine("There is no third number");
+}
