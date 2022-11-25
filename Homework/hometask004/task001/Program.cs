bd51ab8b251dc0b2a2323cd7ab11c1f50a9b84bc
AnnPCMaibenben <ann.erma4kova@gmail.com>
2022-11-25 17:36:46 +0300

    task 1 is ready

---
+++ Homework/hometask004/task001/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите метод, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. Math.Pow использовать нельзя
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+  Console.Write("Введите число   : ");
+  int A = int.Parse(Console.ReadLine()!);
+  
+  Console.Write("Введите степень : ");
+  int B = int.Parse(Console.ReadLine()!);
+  
+  int numberN = 1;
+  for (int i = 0; i < B; i++) 
+  {
+   numberN *= A;
+  }
+
+  Console.WriteLine("{0} ^ {1} = {2}", A, B, numberN);
+
+ 
+
