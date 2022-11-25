8720e44e16081398d9c88224e47e64fea91fd862
AnnPCMaibenben <ann.erma4kova@gmail.com>
2022-11-26 00:49:00 +0300

    second task draft

---
+++ Homework/hometask004/task002/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите метод, который принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число ");
+int x = int.Parse(Console.ReadLine()!);
+
+int[] array = new int[x.ToString().Length];
+for (int i = 0; i < array.Length; i++)
+    {
+    array[i] = x % 10;
+    x = x / 10;
+    }
+ Array.Reverse(array);
+
+Console.WriteLine(string.Join(" ", array));
+Console.ReadKey();  
+ 
