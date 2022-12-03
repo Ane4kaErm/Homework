3fefca2c2b6667fdc6af8c357bb011e7dddea68e
AnnPCMaibenben <ann.erma4kova@gmail.com>
2022-12-03 14:42:09 +0300

    вычитание индексов

--- Homework/hometask006/subtraction/Program.cs
+++ Homework/hometask006/subtraction/Program.cs
@@ -3,35 +3,34 @@ int[] y = { 0, 1, -2, -5, 0, 0, 7, 3 };
 
 int[] Sub(int[] f, int[] g)
 {
-    int powF = f.Length;
-    int powG = g.Length;
+  int powF = f.Length;
+  int powG = g.Length;
 
-    int resultMax = powF;
-    int resultMin = powG;
+  int resultMax = powF;
+  int resultMin = powG;
 
-    if (powG > resultMax)
-    {
-        resultMax = powG;
-        resultMin = powF;
-    }
+  if (powG > resultMax)
+  {
+    resultMax = powG;
+    resultMin = powF;
+  }
 
-    int[] result = new int[resultMax];
+  int[] result = new int[resultMax];
 
-    for (int i = 0; i < resultMin; i++)
-    {
-        result[i] = f[i] - g[i];
-    }
+  for (int i = 0; i < resultMin; i++)
+  {
+    result[i] = f[i] - g[i];
+  }
 
-    for (int i = resultMin; i < resultMax; i++)
-    {
-        if (resultMax == powG) result[i] = g[i];
-        else result[i] = f[i];
-    }
+  for (int i = resultMin; i < resultMax; i++)
+  {
+    if (resultMax == powG) result[i] = g[i] * (-1);
+    else result[i] = f[i];
+  }
 
-    return result;
+  return result;
 }
 
 int[] s = Sub(x, y);
-for (int i = 0; i < s.Length; i++) Console.Write($"{s[i]} ");
-
+for (int i = 0; i<s.Length;i++) Console.Write($"{s[i]} ");
 System.Console.WriteLine();
