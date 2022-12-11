d0fc653d470a5d8a24c61575801c16699b2cd3f2
AnnPCMaibenben <ann.erma4kova@gmail.com>
2022-12-11 20:48:17 +0300

    check array method done

---
+++ Homework/finalTest/Program.cs
@@ -0,0 +1,19 @@
+﻿string[] array = new string[4] {"Hello", "2", "world", ":-)"};
+string[] arrayNew = new string[array.Length];
+
+void CheckArray(string[] array, string[] arrayNew)
+{
+    int count = 0;
+    int length = 3;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= length) 
+        {
+            arrayNew[count] = array[i];
+            count++;
+        }
+    }
+}
+
+
+
